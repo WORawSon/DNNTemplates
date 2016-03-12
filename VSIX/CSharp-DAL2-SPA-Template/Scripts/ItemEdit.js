b2cac043a5afb90754384d86a8084ad04e04121b
WORawSon <wrawson@wortechdesigns.com>
2016-03-11 20:18:43 -0500

    Fix blocking exception addRewriteQueryString
    Added check to help prevent key with null value being sent to decode
    function. Need to be revisited to find the best way to handle the page
    name in the query string. Refactored function calls before they where
    defined, changed single quotes to double. Changed class names to Camel
    Caps. Added line termination.

--- VSIX/CSharp-DAL2-SPA-Template/Scripts/ItemEdit.js
+++ VSIX/CSharp-DAL2-SPA-Template/Scripts/ItemEdit.js
@@ -1,6 +1,8 @@
-﻿var $safeprojectname$ = $safeprojectname$ || {};
+﻿
+var $safeprojectname$ = $safeprojectname$ || {};
 
-$safeprojectname$.itemViewModel = function (moduleId, resx) {
+// Change $safeprojectname$.itemViewModel to $safeprojectname$.ItemViewModel
+$safeprojectname$.ItemViewModel = function(moduleId, resx) {
     var service = {
         path: "$safeprojectname$",
         framework: $.ServicesFramework(moduleId)
@@ -8,50 +10,71 @@ $safeprojectname$.itemViewModel = function (moduleId, resx) {
     service.baseUrl = service.framework.getServiceRoot(service.path) + "Item/";
 
     var id = ko.observable(-1);
-    var name = ko.observable('');
-    var description = ko.observable('');
+    var name = ko.observable("");
+    var description = ko.observable("");
     var assignedUser = ko.observable(-1);
     var userList = ko.observableArray([]);
     var isLoading = ko.observable(false);
 
-    var init = function () {
-        var qs = getQueryStrings();
-        var itemId = qs["tid"];
-        if (itemId) {
-            getItem(itemId);
-        }
-        getUserList();
+    var clear = function() {
+        id("");
+        name("");
+        description("");
+        assignedUser(-1);
     };
 
-    var getQueryStrings = function () {
+    var load = function(data) {
+        // Added line termination.
+        id(data.id);
+        name(data.name);
+        assignedUser(data.assignedUser);
+        description(data.description);
+    };
+
+    var addRewriteQueryString = function(hash, decode) {
+        var path = location.pathname;
+        var queryString = path.substring(path.search("/ctl/") + 1);
+        var keyValues = queryString.split("/");
+
+        for (var i = 0; i < keyValues.length; i += 2) {
+            // Prevent Unpaired Key i.e. Default.aspx from being sent to decode function throwing exception.
+            if (i < key.length - 1) {
+                hash[decode(keyValues[i])] = decode(keyValues[i + 1]);
+            } else {
+                break;
+            }
+        }
+        return hash;
+    };
+
+    var getQueryStrings = function() {
         var assoc = {};
-        var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+        var decode = function(s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
         var queryString = location.search.substring(1);
-        var keyValues = queryString.split('&');
+        var keyValues = queryString.split("&");
 
         for (var i = 0; i < keyValues.length; i++) {
-            var key = keyValues[i].split('=');
+            var key = keyValues[i].split("=");
             if (key.length > 1) {
                 assoc[decode(key[0])] = decode(key[1]);
             }
         }
-
         return addRewriteQueryString(assoc, decode);
     };
 
-    var addRewriteQueryString = function (hash, decode) {
-        var path = location.pathname;
-        var queryString = path.substring(path.search('/ctl/') + 1);
-        var keyValues = queryString.split('/');
-
-        for (var i = 0; i < keyValues.length; i += 2) {
-            hash[decode(keyValues[i])] = decode(keyValues[i + 1])
+    var loadUsers = function(data) {
+        userList.removeAll();
+        var underlyingArray = userList();
+        for (var i = 0; i < data.length; i++) {
+            var result = data[i];
+            // Changed $safeprojectname$.user to $safeprojectname$.User
+            var user = new $safeprojectname$.User(result.id, result.name);
+            underlyingArray.push(user);
         }
-
-        return hash;
+        userList.valueHasMutated();
     };
 
-    var getItem = function (itemId) {
+    var getItem = function(itemId) {
         isLoading(true);
 
         var restUrl = service.baseUrl + itemId;
@@ -59,19 +82,19 @@ $safeprojectname$.itemViewModel = function (moduleId, resx) {
             url: restUrl,
             beforeSend: service.framework.setModuleHeaders,
             dataType: "json"
-        }).done(function (data) {
+        }).done(function(data) {
             if (data) {
                 load(data);
             }
             else {
                 clear();
             }
-        }).always(function (data) {
+        }).always(function(data) {
             isLoading(false);
         });
     };
 
-    var getUserList = function () {
+    var getUserList = function() {
         isLoading(true);
 
         // need to calculate a different Url for User service
@@ -81,26 +104,19 @@ $safeprojectname$.itemViewModel = function (moduleId, resx) {
             beforeSend: service.framework.setModuleHeaders,
             dataType: "json",
             async: false
-        }).done(function (data) {
+        }).done(function(data) {
             if (data) {
                 loadUsers(data);
             }
             else {
                 clear();
             }
-        }).always(function (data) {
+        }).always(function(data) {
             isLoading(false);
         });
     };
 
-    var load = function (data) {
-        id(data.id)
-        name(data.name);
-        assignedUser(data.assignedUser);
-        description(data.description);
-    };
-
-    var save = function () {
+    var save = function() {
         isLoading(true);
         var item = {
             id: id(),
@@ -122,35 +138,27 @@ $safeprojectname$.itemViewModel = function (moduleId, resx) {
             data: JSON.stringify(item),
             beforeSend: service.framework.setModuleHeaders,
             dataType: "json"
-        }).done(function (data) {
+        }).done(function(data) {
             console.log(data);
             dnnModal.closePopUp();
-        }).always(function (data) {
+        }).always(function(data) {
             isLoading(false);
         });
     };
 
-    var loadUsers = function (data) {
-        userList.removeAll();
-        var underlyingArray = userList();
-        for (var i = 0; i < data.length; i++) {
-            var result = data[i];
-            var user = new $safeprojectname$.user(result.id, result.name);
-            underlyingArray.push(user);
-        }
-        userList.valueHasMutated();
-    };
-
-    var clear = function () {
-        id('');
-        name('');
-        description('');
-        assignedUser(-1);
-    };
-
-    var cancel = function () {
+    var cancel = function() {
         dnnModal.closePopUp(false);
     };
+
+    var init = function() {
+        var qs = getQueryStrings();
+        var itemId = qs["tid"];
+        if (itemId) {
+            getItem(itemId);
+        }
+        getUserList();
+    };
+
 
     return {
         id: id,
@@ -165,8 +173,8 @@ $safeprojectname$.itemViewModel = function (moduleId, resx) {
         isLoading: isLoading
     };
 }
-
-$safeprojectname$.user = function (id, name) {
+// Changed user to User
+$safeprojectname$.User = function(id, name) {
     this.id = id;
     this.name = name;
 }
